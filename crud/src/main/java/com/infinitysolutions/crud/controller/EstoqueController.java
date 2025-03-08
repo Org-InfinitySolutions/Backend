0210619ed0685d2bc0b46c8df66c924004368443
MarcoAntonioMS <marco.souza@sptech.school>
2025-03-08 16:44:57 -0300

    Implementação dos CRUD inicial de estoque

--- crud/src/main/java/com/infinitysolutions/crud/controller/EstoqueController.java
+++ crud/src/main/java/com/infinitysolutions/crud/controller/EstoqueController.java
@@ -1,4 +1,46 @@
 package com.infinitysolutions.crud.controller;
+import com.infinitysolutions.crud.mapper.EstoqueMapper;
+import com.infinitysolutions.crud.model.Estoque;
+import com.infinitysolutions.crud.model.dto.EstoqueDtoCriacao;
+import com.infinitysolutions.crud.service.EstoqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/estoque")
 public class EstoqueController {
+    @Autowired
+    private EstoqueService service;
+
+    @PostMapping
+    public ResponseEntity<EstoqueDtoCriacao> criar(@RequestBody EstoqueDtoCriacao dto){
+        Estoque estoque = EstoqueMapper.toEstoque(dto);
+        Estoque estoqueSalvo = service.criar(estoque);
+        return new ResponseEntity<>(EstoqueMapper.toDto(estoqueSalvo), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Estoque>> listar(){
+        return new ResponseEntity<>(service.listar(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Estoque> buscarPorId(@PathVariable Integer id){
+        return new ResponseEntity<>(service.buscarPorId(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Estoque> atualizar(@PathVariable Integer id, @RequestBody EstoqueDtoCriacao dto){
+        Estoque estoque = EstoqueMapper.toEstoque(dto);
+        return new ResponseEntity<>(service.atualizar(id, estoque), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
+        service.deletar(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- crud/src/main/java/com/infinitysolutions/crud/service/EstoqueService.java
+++ crud/src/main/java/com/infinitysolutions/crud/service/EstoqueService.java
@@ -1,4 +1,48 @@
 package com.infinitysolutions.crud.service;
 
+import com.infinitysolutions.crud.model.Estoque;
+import com.infinitysolutions.crud.repository.EstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
+
+@Service
 public class EstoqueService {
+    @Autowired
+    private EstoqueRepository repository;
+
+    public Estoque criar(Estoque estoque) {
+        return repository.save(estoque);
+    }
+
+    public List<Estoque> listar() {
+        return repository.findAll();
+    }
+
+    public Estoque buscarPorId(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Estoque não encontrado para o ID: " + id));
+    }
+
+    public Estoque atualizar(Integer id, Estoque estoqueAtualizado) {
+        Estoque estoqueExistente = buscarPorId(id);
+        if (!estoqueExistente.getId().equals(id)) {
+            throw new IllegalArgumentException("A atualização do ID não é permitida.");
+        }
+        estoqueExistente.setNome(estoqueAtualizado.getNome());
+        estoqueExistente.setNumSerie(estoqueAtualizado.getNumSerie());
+        estoqueExistente.setCategoria(estoqueAtualizado.getCategoria());
+        return repository.save(estoqueExistente);
+    }
+
+    public void deletar(Integer id) {
+        Estoque estoque = buscarPorId(id);
+        repository.delete(estoque);
+    }
 }
+
+
+
+
