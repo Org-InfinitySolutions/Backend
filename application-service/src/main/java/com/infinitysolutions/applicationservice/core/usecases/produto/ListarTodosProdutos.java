6b03d2cb6b1d2175caa8d77fc1c233336062a20f
mhssouza <matheus.santos096@sptech.school>
2025-11-01 15:41:03 -0300

    criação pag. produtos

--- application-service/src/main/java/com/infinitysolutions/applicationservice/core/usecases/produto/ListarTodosProdutos.java
+++ application-service/src/main/java/com/infinitysolutions/applicationservice/core/usecases/produto/ListarTodosProdutos.java
@@ -2,6 +2,7 @@ package com.infinitysolutions.applicationservice.core.usecases.produto;
 
 import com.infinitysolutions.applicationservice.core.domain.produto.Produto;
 import com.infinitysolutions.applicationservice.core.gateway.ProdutoGateway;
+import com.infinitysolutions.applicationservice.core.valueobject.PageResult;
 
 import java.util.List;
 
@@ -15,5 +16,9 @@ public class ListarTodosProdutos {
 
     public List<Produto> execute(boolean isAdmin) {
         return produtoGateway.findAll(isAdmin);
+    }
+
+    public PageResult<Produto> execute(boolean isAdmin, int offset, int limit) {
+        return produtoGateway.findAllPaginated(isAdmin, offset, limit);
     }
 }
